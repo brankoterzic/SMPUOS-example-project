4b7730a1f2d7609ca722b5b5b534fb29072ef22f
Branko Terzic <brankoterzic91@gmail.com>
2016-11-02 17:08:18 +0100

    Feign client part1. Added.

--- Ribbon/src/main/java/rs/uns/acs/ftn/RibbonApplication.java
+++ Ribbon/src/main/java/rs/uns/acs/ftn/RibbonApplication.java
@@ -7,9 +7,13 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,6 +31,7 @@ class MyConfiguration {
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableFeignClients
 public class RibbonApplication {
 
 	@Autowired
@@ -35,6 +40,9 @@ public class RibbonApplication {
 	@Autowired
 	private RestTemplate rest;
 	
+	@Autowired
+	private Hello hello;// feign interface instance
+	
 	@RequestMapping("/choose")// Do load-balancing among the user-service instances
 	public ServiceInstance choose(){
 		return lb.choose("user-service");
@@ -42,7 +50,9 @@ public class RibbonApplication {
 	
 	@RequestMapping(value = "/", produces = "text/html")
 	public String produce(){
-		String s = rest.getForObject("http://user-service/hello/?name={name}", String.class, "IGIS");
+		//String s = rest.getForObject("http://user-service/users/hello/?name={name}", String.class, "IGIS");//using rest template
+		
+		String s = hello.hello("IGIS"); // feign REST client call
 		return String.format("<html>"
 								+ "<head>"
 									+ "<title>TEST</title>"
@@ -51,6 +61,12 @@ public class RibbonApplication {
 										+ "<h1>%s</h1>"
 									+ "</body>"
 								+ "</html>", s);
+	}
+	
+	@FeignClient("user-service")
+	public interface Hello {
+		@RequestMapping(value = "users/hello", method = RequestMethod.GET)
+		String hello(@RequestParam(name = "name") String name);
 	}
 	
 	public static void main(String[] args) {
--- UserServiceDemo/src/main/java/rs/uns/acs/ftn/UserServiceDemoApplication.java
+++ UserServiceDemo/src/main/java/rs/uns/acs/ftn/UserServiceDemoApplication.java
@@ -3,13 +3,25 @@ package rs.uns.acs.ftn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
  //This service is used to demonstrate how ribbon round robin works with load-balancing 
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
+@RequestMapping("users")
 public class UserServiceDemoApplication {
 
+	@RequestMapping(value = "/hello")
+	public String hello(
+			
+			@RequestParam(name = "name") String name){
+		return "Hello " + name + " " + "IGIS 8083";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceDemoApplication.class, args);
 	}
