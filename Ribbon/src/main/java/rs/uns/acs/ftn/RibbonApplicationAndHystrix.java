6f0c5abe84f3406f4ec2ff63da78cd76f59cee2e
Branko Terzic <brankoterzic91@gmail.com>
2016-12-29 13:38:27 +0100

    Ribbon configuration updated.

--- Ribbon/src/main/java/rs/uns/acs/ftn/RibbonApplicationAndHystrix.java
+++ Ribbon/src/main/java/rs/uns/acs/ftn/RibbonApplicationAndHystrix.java
@@ -7,20 +7,12 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import rs.uns.acs.ftn.config.RibbonConfig;
 import rs.uns.acs.ftn.services.HystrixService;
-
-@Configuration
-@RibbonClient(name = "user-service", configuration = RibbonConfig.class)
-class TestConfiguration {
-}
 
 // @SpringBootApplication
 // @EnableDiscoveryClient
@@ -50,12 +42,11 @@ public class RibbonApplicationAndHystrix {
 
 	@RequestMapping(value = "/", produces = "text/html")
 	public String produce() {
-		String s = rest.getForObject("http://user-service/users/hello", String.class);// using rest template
+		// String s = rest.getForObject("http://user-service/users/hello", String.class);// using rest template
 
 		// String s = hello.hello(); // feign REST client call
 
-		// String s = hystrixService.hello(); // call fegin client hello method
-		// throught hystrix call
+		 String s = hystrixService.hello(); // call fegin client hello method throught hystrix call
 
 		return String.format("<html>" + "<head>" + "<title>TEST</title>" + "</head>" + "<body>" + "<h1>%s</h1>"
 				+ "</body>" + "</html>", s);
--- Ribbon/src/main/java/rs/uns/acs/ftn/config/RibbonConfig.java
+++ Ribbon/src/main/java/rs/uns/acs/ftn/config/RibbonConfig.java
@@ -1,5 +1,6 @@
 package rs.uns.acs.ftn.config;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,13 +8,31 @@ import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 
 @Configuration
-public class RibbonConfig{
+class RibbonConfig{
     @Bean
     public IRule ribbonRule() {
+    	
+    	/*This rule simply choose servers by round robin. It is often used as the default rule or fallback of more advanced rules.*/
         return new RandomRule();
+        
+        /*This rule will skip servers that are deemed "circuit tripped" or with high concurrent connection count.
+         * By default, an instance is circuit tripped if the RestClient fails to make a connection to it for the last three times.
+         *  Once an instance is circuit tripped, it will remain in this state for 30 seconds before the circuit is deemed as closed again. 
+         *  However, if it continues to fail connections, it will become "circuit tripped" again and the wait time for it to become "circuit closed" will increase exponentially to the number of consecutive failures.
+         */
         //return new AvailabilityFilteringRule()
-    	//return new BestAvailableRule();
+        
+        /*For this rule, each server is given a weight according to its average response time. 
+         *The longer the response time, the less weight it will get. 
+         *The rule randomly picks a server where the possibility is determined by server's weight.*/
     	//return new WeightedResponseTimeRule();
+        
+    	//return new BestAvailableRule();
     	//return new ZoneAvoidanceRule();
     }
+}
+
+@Configuration
+@RibbonClient(name = "user-service", configuration = RibbonConfig.class)
+class TestConfiguration {
 }
