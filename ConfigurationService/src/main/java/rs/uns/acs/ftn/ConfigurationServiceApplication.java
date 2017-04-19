554b51a309da8ba53d02ad16914089aa929d9325
Branko Terzic <brankoterzi91@gmail.com>
2017-04-19 15:26:33 +0200

    Changing Congiguration files and moving them to separate repository

--- ConfigurationService/src/main/java/rs/uns/acs/ftn/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/rs/uns/acs/ftn/ConfigurationServiceApplication.java
@@ -1,9 +1,11 @@
 package rs.uns.acs.ftn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableAutoConfiguration
 public class ConfigurationServiceApplication 
 {
     public static void main( String[] args )
--- PaymentService/src/main/java/rs/uns/acs/ftn/PaymentServiceApplication.java
+++ PaymentService/src/main/java/rs/uns/acs/ftn/PaymentServiceApplication.java
@@ -1,9 +1,11 @@
 package rs.uns.acs.ftn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAutoConfiguration
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PaymentServiceApplication {
