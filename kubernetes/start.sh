a53b0df190de08b00efc69e96dcd386b91eb8ba3
Branko Terzic <branko.terzic@gamecredits.com>
2018-05-12 12:13:59 +0200

    Spring version update and Kubernetes startup script update

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -aq)
-docker rmi -f $(docker images -a -q)
-
 minikube start
 
 #Setup Minikube to run local Docker images ad  build the Docker image
 eval $(minikube docker-env)
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -aq)
+docker rmi -f $(docker images -a -q)
+
 cd ../SpringDataMongoDB/ && docker build -t user-service:latest .
 
 cd ../kubernetes/mongodb && kubectl create -f deployment.yml && kubectl create -f service.yml
 
 cd ../user-service && kubectl create -f deployment.yml && kubectl create -f service.yml 
+
+minikube service user-service
